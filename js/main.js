7eb922490850a3fba69e241e992de454bc89dbe4
VitalijusS <v.stifanovicius@gmail.com>
2024-07-10 17:03:45 +0300

    todo

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const h1DOM = document.querySelector('h1');
+const formDOM = document.forms[0];
+const textImputDOM = document.querySelector('input');
+const submitButtonDOM = document.querySelector('button');
+const listDOM = document.querySelectorAll('.list')[1];
+const emptyDOM = document.querySelector('.empty');
+let listIndex = 0;
+
+
+submitButtonDOM.addEventListener('click', event => {
+    event.preventDefault();
+    if (textImputDOM.value.trim() !== '') {
+        emptyDOM.style.display = "none";
+        const HTML = `
+        <article id="${listIndex}" class ="item">
+        <div class="text">${textImputDOM.value}</div>
+        <div class="action">
+        <button class="${listIndex} done">Done</button>
+        <div class="diviter">|</div>
+        <button class ="${listIndex} edit">Edit</button>
+        <button class="${listIndex} delete">Delete</button>
+        </div>
+        </article>
+        `;
+        listDOM.innerHTML += HTML;
+    }
+    listIndex++;
+    textImputDOM.value = '';
+    buttonDone();
+    buttonDelete();
+    buttonEdit();
+});
+function buttonDone() {
+    const btns = document.querySelectorAll('.done');
+    for (const btn of btns) {
+        btn.addEventListener('click', () => {
+            const index = parseInt(btn.className.split(' ')[0]);
+            document.getElementById(index).style.background = 'green';
+        })
+    }
+}
+function buttonEdit() {
+    const btns = document.querySelectorAll('.edit');
+    for (const btn of btns) {
+        btn.addEventListener('click', () => {
+            const index = parseInt(btn.className.split(' ')[0]);
+            document.getElementById(index).style.background = 'white';
+        })
+    }
+}
+function buttonDelete() {
+    const btns = document.querySelectorAll('.delete');
+    for (const btn of btns) {
+        btn.addEventListener('click', () => {
+            const index = parseInt(btn.className.split(' ')[0]);
+            document.getElementById(index).outerHTML = '';
+            if (document.querySelectorAll('.item').length === 0) {
+                emptyDOM.style.display = "block";
+            }
+        })
+    }
+}
